790340ae65303653dd8b19b563c831034265c993
Franco Zanini <francozanini1995@gmail.com>
2023-12-23 11:25:18 +0100

    prettier + tailwind fix

--- app/root.tsx
+++ app/root.tsx
@@ -8,10 +8,10 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import stylesheet from "~/tailwind.css";
+import stylesheet from "~/globals.css";
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: stylesheet }] : []),
+  { rel: "stylesheet", href: stylesheet },
 ];
 
 export default function App() {

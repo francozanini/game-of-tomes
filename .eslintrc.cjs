4f2abd2d9d0a4c959d37fc2607c9fbbfeb5177d7
Franco Zanini <francozanini1995@gmail.com>
2023-12-25 12:18:52 +0100

    remove airbnb

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -33,9 +33,8 @@ module.exports = {
         "plugin:react/jsx-runtime",
         "plugin:react-hooks/recommended",
         "plugin:jsx-a11y/recommended",
-    'plugin:react/recommended',
-    'airbnb',
-    'plugin:prettier/recommended',
+        'plugin:react/recommended',
+        'plugin:prettier/recommended',
       ],
       settings: {
         react: {
